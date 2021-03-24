677c29f012fa4b3966d805466d6c925a7b7900d4
jennyyen <yenjenny090@gmail.com>
2021-03-24 17:06:01 +0800

    def function_january_first_day (year)

--- function.py
+++ function.py
@@ -1,53 +1,59 @@
 month_day = 0
-initialyear = 1582
 distinguish = 0
-j_f_d = 5
-m_f_d = 0
+
+month_first_day = 0
 mmonth = 1
-year = eval(input("請輸入年"))
-month = eval(input("請輸入月"))
 allmonth = ["JAN", 'FEB', 'MAR', 'APR', 'MAY',
             'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
 
-
-'''def everymonthfirstday(year, month):
-    month_day = 0
+'''month_day = 0
 initial_year = 1582
 distinguish = 0
 january_first_day = 5
 month_first_day = 0
 initialmonth = 1'''
+
 year = eval(input('請輸入年份'))
 month = eval(input('請輸入月份'))
-date = eval(input('請輸入日期'))
-def distinguish_leapyear(year):
-    distinguish = 0
-    if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:#leapyear
-        distinguish = 1  
-    elif year % 4 == 0 and year % 100 == 0:#leapyear
+#date = eval(input('請輸入日期'))
+
+
+def function_january_first_day (year):
+    global initial_year
+    global january_first_day 
+    global month_first_day
+    global month_day
+    global distinguish 
+    initial_year=1582
+    january_first_day = 5
+    month_first_day = 0
+    month_day = 0
+    while 1:
         distinguish = 0
-    elif year % 4 == 0:
-        distinguish = 1
-    else:
-        distinguish = 0 #判斷平閏年# Bissextile Year=1 #Common Year=0 
-    return distinguish
-print(distinguish_leapyear(year))
+        if initial_year % 4 == 0 and initial_year % 100 == 0 and initial_year % 400 == 0:#leapyear
+            distinguish = 1  
+        elif initial_year % 4 == 0 and initial_year % 100 == 0:#leapyear
+            distinguish = 0
+        elif initial_year % 4 == 0:
+            distinguish = 1
+        else:
+            distinguish = 0 #判斷平閏年# Bissextile Year=1 #Common Year=0 
+        if month == 1 or month ==3 or month ==5 or month ==7 or month ==8 or month ==10 or month ==12:
+            month_day = 31
+        elif month==2:
+            if  distinguish == 0:
+                month_day = 28
+            else:
+                month_day = 29
+        else:
+            month_day = 30
+        if year == initial_year:
+            break
+        elif distinguish == 1:
+            january_first_day = (january_first_day+2)%7
+        else:
+            january_first_day = (january_first_day+1)%7 
+        initial_year = initial_year+1
+    return january_first_day
 
-
- #if month == 1 or month ==3 or month ==5 or month ==7 or month ==8 or month ==10 or month ==12:
-        #month_day = 31
-    #elif month==2 and distinguish == 0:
-        #month_day = 28
-    #elif month==2 and distinguish == 1:
-        #month_day = 29
-    #else:
-        #month_day = 30
-    #if year == initial_year:
-        #break
-    #elif distinguish == 1:
-        #january_first_day = (january_first_day+2)%7
-    #else:
-        #january_first_day = (january_first_day+1)%7
-    #initial_year = initial_year+1'''
-
-#month_first_day = january_first_day   
+print(function_january_first_day(year))
--- 萬年曆.py
+++ 萬年曆.py
@@ -25,7 +25,7 @@ elif mode == 3:
     month = eval(input('請輸入月份'))
     date = eval(input('請輸入日期'))
     print('進入日曆模式')
-while 1:
+while 1:#找到distinguish值和
     if initial_year % 4 == 0 and initial_year % 100 == 0 and initial_year % 400 == 0:#leapyear
         distinguish = 1  
     elif initial_year % 4 == 0 and initial_year % 100 == 0:#leapyear
@@ -36,10 +36,11 @@ while 1:
         distinguish = 0 #判斷平閏年# Bissextile Year=1 #Common Year=0 
     if month == 1 or month ==3 or month ==5 or month ==7 or month ==8 or month ==10 or month ==12:
         month_day = 31
-    elif month==2 and distinguish == 0:
-        month_day = 28
-    elif month==2 and distinguish == 1:
-        month_day = 29
+    elif month==2:
+        if  distinguish == 0:
+            month_day = 28
+        else:
+            month_day = 29
     else:
         month_day = 30
     if year == initial_year:
@@ -62,7 +63,7 @@ while 1:
         month_first_day = (month_first_day+28) % 7
     elif initialmonth == 5 or initialmonth == 7 or initialmonth ==10 or initialmonth ==12:
         month_first_day = (month_first_day+30) % 7
-    elif initialmonth == 2 or initialmonth == 4 or initialmonth == 6 or initialmonth == 8 or i2nitialmonth ==9 or initialmonth ==11:
+    elif initialmonth == 2 or initialmonth == 4 or initialmonth == 6 or initialmonth == 8 or initialmonth ==9 or initialmonth ==11:
         month_first_day = (month_first_day+31) % 7
     if initialmonth == month:
         break
